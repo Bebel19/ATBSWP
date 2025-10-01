8e4a40a7b8e1975a1de6a78a02f161edb2cccd58
Bebel19 <belgnaoui.othman@gmail.com>
2025-10-01 11:08:59 +0200

    Added Table printer project

---
+++ Table Printer/Table Printer.py
@@ -0,0 +1,62 @@
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+['Alice', 'Bob', 'Carol', 'David'],
+['dogs', 'cats', 'moose', 'goose']]
+
+def  findLongestString(table):
+    """
+
+    :param table:
+    :return: List of lengths of the longest string of each row of a table
+    """
+
+
+    longest = {
+    "rowIndex" : 0,
+    "cellIndex": 0,
+    "length": 0,
+    }
+
+    res = []
+    lengthsList = []
+
+    for rowIndex, row in enumerate(table):
+        for cellIndex, cell in enumerate(row):
+
+            cellLength = len(cell)
+
+            if cellLength > longest["length"]:
+
+                longest["length"] = cellLength
+                longest["rowIndex"] = rowIndex
+                longest["cellIndex"] = cellIndex
+
+        res.append(longest)
+        longest = {
+            "rowIndex": 0,
+            "cellIndex": 0,
+            "length": 0,
+        }
+
+    for dict in res:
+        lengthsList.append(dict["length"])
+
+    return lengthsList
+
+def printTable(table):
+    colWidths = [0] * len(table)
+
+    lengthsList = findLongestString(table)
+    longestLenghts = max(lengthsList)
+
+    for colIndex in range(len(table[0])):
+        for lineIndex in range(len(colWidths)):
+            print(f"{table[lineIndex][colIndex]}".center(longestLenghts), end="\t")
+
+        print("\n")
+
+
+
+
+print(findLongestString(tableData))
+
+printTable(tableData)
